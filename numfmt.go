5cfad6c90308b99974c2d7eb5dbf64c7d97c2791
Jack Christensen <jack@jackchristensen.com>
2021-02-06 13:18:01 -0600

    Initial commit

---
+++ numfmt.go
@@ -0,0 +1,113 @@
+package numfmt
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/shopspring/decimal"
+)
+
+type Rounder struct {
+	Places int32
+}
+
+func (r *Rounder) Round(d decimal.Decimal) decimal.Decimal {
+	return d.Round(r.Places)
+}
+
+type Formatter struct {
+	GroupSeparator   string // Default: ","
+	GroupSize        int    // Default: 3
+	DecimalSeparator string // Default: "."
+	Rounder          *Rounder
+}
+
+func (f *Formatter) Format(v interface{}) string {
+	switch v := v.(type) {
+	case decimal.Decimal:
+		return f.FormatDecimal(v)
+	case string:
+		d, err := decimal.NewFromString(v)
+		if err != nil {
+			return fmt.Sprint(v)
+		}
+		return f.FormatDecimal(d)
+	default:
+		s := fmt.Sprint(v)
+		d, err := decimal.NewFromString(s)
+		if err != nil {
+			return s
+		}
+		return f.FormatDecimal(d)
+	}
+}
+
+func (f *Formatter) FormatDecimal(d decimal.Decimal) string {
+	if f.Rounder != nil {
+		d = d.Round(f.Rounder.Places)
+	}
+	return f.formatNumberString(d.String())
+}
+
+func (f *Formatter) formatNumberString(s string) string {
+	parts := strings.SplitN(s, ".", 2)
+	intPart := parts[0]
+	var fracPart string
+	if len(parts) == 2 {
+		fracPart = parts[1]
+	}
+
+	neg := false
+	if intPart[0] == '-' {
+		neg = true
+		intPart = intPart[1:]
+	}
+
+	sb := &strings.Builder{}
+	if neg {
+		sb.WriteString("-")
+	}
+
+	groupSeparator := ","
+	if f.GroupSeparator != "" {
+		groupSeparator = f.GroupSeparator
+	}
+	groupSize := 3
+	if f.GroupSize != 0 {
+		groupSize = f.GroupSize
+	}
+	writeSeparateGroups(sb, intPart, groupSeparator, groupSize)
+
+	decimalSeparator := "."
+	if f.DecimalSeparator != "" {
+		decimalSeparator = f.DecimalSeparator
+	}
+	if len(fracPart) != 0 {
+		sb.WriteString(decimalSeparator)
+		sb.WriteString(fracPart)
+	}
+
+	return sb.String()
+}
+
+func writeSeparateGroups(sb *strings.Builder, num, groupSeparator string, groupSize int) {
+	if len(groupSeparator) == 0 || groupSize == 0 || len(num) <= groupSize {
+		sb.WriteString(num)
+		return
+	}
+
+	sepCount := len(num) / groupSize
+	numIdx := len(num) % groupSize
+	if numIdx == 0 {
+		numIdx = groupSize
+		sepCount--
+	}
+	sb.WriteString(num[:numIdx])
+
+	for i := 0; i < sepCount; i++ {
+		sb.WriteString(groupSeparator)
+		lastNumIdx := numIdx
+		numIdx += groupSize
+		sb.WriteString(num[lastNumIdx:numIdx])
+	}
+}
---
+++ numfmt_test.go
@@ -0,0 +1,74 @@
+package numfmt_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/jackc/numfmt"
+	"github.com/shopspring/decimal"
+)
+
+type testFormatter numfmt.Formatter
+
+func (f *testFormatter) String() string {
+	parts := []string{}
+	if f.GroupSeparator != "" {
+		parts = append(parts, fmt.Sprintf(`GroupSeparator: "%s"`, f.GroupSeparator))
+	}
+	if f.GroupSize != 0 {
+		parts = append(parts, fmt.Sprintf("GroupSize: %d", f.GroupSize))
+	}
+	if f.DecimalSeparator != "" {
+		parts = append(parts, fmt.Sprintf(`DecimalSeparator: "%s"`, f.DecimalSeparator))
+	}
+	if f.Rounder != nil {
+		parts = append(parts, fmt.Sprintf(`Rounder: {Places: %d}`, f.Rounder.Places))
+	}
+
+	return "&Formatter{" + strings.Join(parts, ", ") + "}"
+}
+
+func TestFormatterFormat(t *testing.T) {
+	for i, tt := range []struct {
+		formatter *numfmt.Formatter
+		arg       interface{}
+		expected  string
+	}{
+		// Defaults
+		{&numfmt.Formatter{}, "0", "0"},
+		{&numfmt.Formatter{}, "1", "1"},
+		{&numfmt.Formatter{}, "12", "12"},
+		{&numfmt.Formatter{}, "123", "123"},
+		{&numfmt.Formatter{}, "1234", "1,234"},
+		{&numfmt.Formatter{}, "12345", "12,345"},
+		{&numfmt.Formatter{}, "123456", "123,456"},
+		{&numfmt.Formatter{}, "12345678901234567890", "12,345,678,901,234,567,890"},
+		{&numfmt.Formatter{}, "1.0", "1"},
+		{&numfmt.Formatter{}, "1.2", "1.2"},
+		{&numfmt.Formatter{}, "12345.6789", "12,345.6789"},
+
+		{&numfmt.Formatter{DecimalSeparator: ","}, "1.2", "1,2"},
+		{&numfmt.Formatter{GroupSeparator: " "}, "1234", "1 234"},
+		{&numfmt.Formatter{GroupSize: 1}, "1234", "1,2,3,4"},
+
+		{&numfmt.Formatter{Rounder: &numfmt.Rounder{Places: 0}}, "1234.1", "1,234"},
+		{&numfmt.Formatter{Rounder: &numfmt.Rounder{Places: 0}}, "1234.5", "1,235"},
+		{&numfmt.Formatter{Rounder: &numfmt.Rounder{Places: 0}}, "1234.9", "1,235"},
+		{&numfmt.Formatter{Rounder: &numfmt.Rounder{Places: 3}}, "1234.5678", "1,234.568"},
+		{&numfmt.Formatter{Rounder: &numfmt.Rounder{Places: -2}}, "1234.5678", "1,200"},
+
+		// Different argument type tests
+		{&numfmt.Formatter{}, 1234, "1,234"},
+		{&numfmt.Formatter{}, 1234.0, "1,234"},
+		{&numfmt.Formatter{}, decimal.RequireFromString("1234"), "1,234"},
+
+		// Not a number
+		{&numfmt.Formatter{}, "foobar", "foobar"},
+	} {
+		actual := tt.formatter.Format(tt.arg)
+		if tt.expected != actual {
+			t.Errorf("%d. expected formatting %v with %v to return %v, but got %v", i, tt.arg, (*testFormatter)(tt.formatter), tt.expected, actual)
+		}
+	}
+}
